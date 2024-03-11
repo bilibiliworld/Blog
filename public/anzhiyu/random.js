20b2840aa6b7038c5e2b85737d97be9a8c57357d
bilibiliworld <fulafulovecina@gmail.com>
2024-03-11 20:14:04 +0800

    1.5

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/07/hello-world/"];function toRandomPost(){
+var posts=["2024/03/11/test/","2024/03/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
